3a02b480f6015c0f9d36de020368fbe5fa7d2968
eruud <eruud@meinergy.com>
2017-04-19 13:16:56 -0500

    Test

--- R/human_summary.r
+++ R/human_summary.r
@@ -1,5 +1,6 @@
 ## this is our entry point from opencpu
 #'@export
+#'@import jsonlite dplyr ggvis
 process_cal_human <- function(data, params, ...) {
   ## some notes on syntax with this function.
   ## some of these intermediate functions might return things of interest?
