9142b47d50be84307ed3707abbe34ffeaf581b63
eruud <eruud@meinergy.com>
2019-06-14 08:30:30 -0500

    Test

--- R/regression_rmr.R
+++ R/regression_rmr.R
@@ -8,7 +8,7 @@ calculate_rmr <- function(data, sampling_seconds) {
     if (is.null(data$Activity[i])) {
       data$Activity[i] <- 0
     } else {
-      stop(data$Activity)
+      stop(data$Activity,i)
     }
     #else if (is.na(data$Activity[i])) {
     #  data$Activity[i] <- 0
